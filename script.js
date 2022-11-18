24a2e94672d88ff7f46444d337b027ab3b23429c
Pick1100 <109476582+Pick1100@users.noreply.github.com>
2022-11-18 16:23:02 -0700

    Fixed some bugs based on incorrect ; and ,

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-let timeEl = 24
-let timeElId = 'timeEl'
-let 
-$(function () {
-  $(onclick).each(function () {
-    localStorage.setItem();
-    $(this).click();
 
+let timeEl = 0;
+let timeElId = 'timeEl';
+let currentTime = 0;
+
+  $(document).ready(function() {
+  $(".btnSave").on( "click", function() {
+    var myval = $('.description').val();
+    console.log(myVal);
+    let i = localStorage.length;
+    localStorage.setItem('description', myval);
+  });
+// localStorage.setItem.description
+//     key,Value,
+
+if (timeEl < currentTime){
+  $(this).addClass('past');
+  $(this).removeClass('current');
+  $(this).removeClass('future');
+} else if(timeEl == currentTime){
+  $(this).addClass('current');
+  $(this).removeClass('past');
+  $(this).removeClass('future');
+} else if(timeEl > currentTime){
+  $(this).addClass('future');
+  $(this).removeClass('present');
+  $(this).removeClass('past');
+}
+
+// }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -28,4 +50,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+})
